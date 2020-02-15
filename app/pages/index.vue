8a9f34ffdfde8a44193d3a6c988472adb7ad4a2f
laPepita <fab_giabardo@hotmail.com>
2020-02-15 17:26:21 +1100

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,13 +9,32 @@
           
           
           
-          
+          <!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://fun.us4.list-manage.com/subscribe/post?u=ed902414073f244989e19ffed&amp;id=e5799a0663" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ed902414073f244989e19ffed_e5799a0663" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
 
           
           
           
           
-          
+          <!--
           <form
             v-else
             @submit.prevent="handleSubmit"
@@ -45,6 +64,7 @@
               Sign Up
             </button>
           </form>
+          -->
         </div>
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
