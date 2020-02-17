4bcd5e1ef4b1a4d2c1817bf3a8f13ea305236f19
laPepita <fab_giabardo@hotmail.com>
2020-02-17 19:54:49 +1100

    index. change forms (no mailchimp)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,35 +4,34 @@
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
-        <div class="mb-12 xl:mb-0">
+        <div class="mb-12 xl:mb-0">          
+          
+          
           <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
-          
-          
-          
-          <!-- Begin Mailchimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
-	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
+          <form
+            v-else
+            @submit.prevent="handleSubmit"
+            name="signups"
+            netlify
+            class="flex items-center border-b border-b-2 border-blue-400 py-2"
+          >
+            <input
+              ref="emailInput"
+              v-model="form.email"
+              class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              name="email"
+              placeholder="your@email.com"
+              aria-label="Email address"
+            />
 
-<form action="https://fun.us4.list-manage.com/subscribe/post?u=ed902414073f244989e19ffed&amp;id=e5799a0663" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate="novalidate" class="validate"><div id="mc_embed_signup_scroll"><input type="email" value="" name="EMAIL" id="mce-EMAIL" placeholder="your@email.com" required="required" class="email" style="
-    border-left: none;
-    border-top: none;
-    border-right: none;
-    border-color: #3796a7;
-    border-radius: 0px;
-"> <div aria-hidden="true" style="position: absolute; left: -5000px;"><input type="text" name="b_ed902414073f244989e19ffed_e5799a0663" tabindex="-1" value=""></div> <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" style="
-    color: white;
-    background-color: #3796a7;
-"></div></div></form>
-
-
-</div>
-
-<!--End mc_embed_signup-->
+            <button
+              class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </form>
 
           
           
